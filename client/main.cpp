af77edd000d62c7feab8a1a80daaa0029515a028
min-38 <mindino03@gmail.com>
2024-09-27 21:29:58 +0900

    client/Work#1 - 서버 IP, Port 입력 받기
    실행 command line에 ip, port 입력 시 입력 한 접속 정보대로 실행

--- client/main.cpp
+++ client/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <mutex>
 #include <unistd.h>
 #include <arpa/inet.h>
 
@@ -11,64 +10,73 @@
 
 std::mutex mtx;
 
-void receiveMessages(int client_socket) {
+void receiveMessages(int* client_fd) {
     char buffer[1024];
-    while (true) {
-        std::lock_guard<std::mutex> guard(mtx);
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+    int bytes_received;
+    do {
+        bytes_received = recv(*client_fd, buffer, sizeof(buffer), 0);
         if (bytes_received > 0) {
             buffer[bytes_received] = '\0';
             std::cout << "Message> " << buffer << std::endl;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    } while(bytes_received > 0);
 }
 
-void client() {
+void sendMessages(int *client_fd) {
+    std::string message;
+    std::cout << "나> " << std::flush;
+    std::getline(std::cin, message);
+    send(*client_fd, message.c_str(), message.size(), 0);
+}
+
+void client(sockaddr_in *server_addr) {
+    // socket 설정
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(client_fd == -1) {
-        std::cerr << "Failed to create socket.\n";
+        std::cerr << "[에러] 소켓 생성 실패.\n";
         return;
     }
 
-    sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
+    if(connect(client_fd, (sockaddr*)server_addr, sizeof(*server_addr)) == -1) {
+        std::cerr << "[에러] 서버와 연결이 되지 않음.\n";
         return;
     }
 
-    if(connect(client_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        std::cerr << "Failed to connect to server.\n";
-        return;
-    }
-
-    std::thread receive_thread(receiveMessages, client_fd);
+    std::thread receive_thread(receiveMessages, &client_fd);
     receive_thread.detach();
-
-    std::string message;
-    while (true) {
-        std::lock_guard<std::mutex> guard(mtx);
-
-        if(!receive_thread.joinable()) {
-            std::cout << "나> ";
-            std::getline(std::cin, message);
-            send(client_fd, message.c_str(), message.size(), 0);
-            
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
+    std::thread* send_thread;
+    while(true) {
+        sendMessages(&client_fd);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-
+    
     close(client_fd);
 }
 
-int main() {
-    // 클라이언트 스레드를 실행
-    std::thread client_thread(client);
+int main(int argc, char* argv[]) {
+    std::string server_ip = "";
+    int server_port = PORT;
+
+    if(argc >= 2) {
+        server_ip = argv[1];
+        server_port = std::stoi(argv[2]);
+    }
+
+    // 서버 설정
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    if(server_ip == "")
+        server_addr.sin_addr.s_addr = IPADDR;
+    else
+        if(inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr.s_addr) <= 0) {
+            std::cerr << "IP 형식과 맞지 않습니다: " << argv[1] << std::endl;
+            return -1;
+        }
+
+    // client thread를 실행
+    std::thread client_thread(client, &server_addr);
     client_thread.join();  // 클라이언트 스레드가 끝날 때까지 기다림
 
     return 0;
